2ac371fc8764140a44a49f05e94c9e2521927805
kutun0901 <tuan.tq2433@gmail.com>
2024-06-03 10:05:46 -0700

    forgot password api

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,12 +8,12 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-	host: 'smtp.gmail.com',
-	port: 587,
-	auth: {
-		user: process.env.USERNAME_EMAIL,
-		pass: process.env.PASSWORD_EMAIL,
-	},
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+        user: process.env.USERNAME_EMAIL,
+        pass: process.env.PASSWORD_EMAIL,
+    },
 });
 
 const getJsonWebToken = async (email, id) => {
@@ -30,41 +30,41 @@ const getJsonWebToken = async (email, id) => {
 }
 
 const handleSendMail = async (val) => {
-	try {
-		await transporter.sendMail(val);
+    try {
+        await transporter.sendMail(val);
 
-		return 'OK';
-	} catch (error) {
-		return error;
-	}
+        return 'OK';
+    } catch (error) {
+        return error;
+    }
 };
 
 const verification = asyncHandle(async (req, res) => {
-	const { email } = req.body;
+    const { email } = req.body;
 
-	const verificationCode = Math.round(1000 + Math.random() * 9000);
+    const verificationCode = Math.round(1000 + Math.random() * 9000);
 
-	try {
-		const data = {
-			from: `"Support EventHub Appplication" <${process.env.USERNAME_EMAIL}>`,
-			to: email,
-			subject: 'Verification email code',
-			text: 'Your code to verification email',
-			html: `<h1>${verificationCode}</h1>`,
-		};
+    try {
+        const data = {
+            from: `"Support EventHub Appplication" <${process.env.USERNAME_EMAIL}>`,
+            to: email,
+            subject: 'Verification email code',
+            text: 'Your code to verification email',
+            html: `<h1>${verificationCode}</h1>`,
+        };
 
-		await handleSendMail(data);
+        await handleSendMail(data);
 
-		res.status(200).json({
-			message: 'Send verification code successfully!!!',
-			data: {
-				code: verificationCode,
-			},
-		});
-	} catch (error) {
-		res.status(401);
-		throw new Error('Can not send email');
-	}
+        res.status(200).json({
+            message: 'Send verification code successfully!!!',
+            data: {
+                code: verificationCode,
+            },
+        });
+    } catch (error) {
+        res.status(401);
+        throw new Error('Can not send email');
+    }
 });
 
 const register = asyncHandle(async (req, res) => {
@@ -107,11 +107,11 @@ const register = asyncHandle(async (req, res) => {
 const login = asyncHandle(async (req, res) => {
 
     // console.log(req.body)
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
     const existingUser = await UserModel.findOne({ email });
 
-    if (!existingUser){
+    if (!existingUser) {
         res.status(403)
         throw new Error('User not found')
         // return next(new Error('User not found')) pass to next errorHandler
@@ -119,7 +119,7 @@ const login = asyncHandle(async (req, res) => {
 
     const isMatchPassword = await bcrypt.compare(password, existingUser.password)
 
-    if (!isMatchPassword){
+    if (!isMatchPassword) {
         res.status(401)
         throw new Error('email or password is not correct!')
     }
@@ -137,8 +137,55 @@ const login = asyncHandle(async (req, res) => {
     // res.send('fafa')
 })
 
+const forgotPassword = asyncHandle(async (req, res) => {
+	const { email } = req.body;
+
+	const randomPassword = Math.round(100000 + Math.random() * 99000);
+
+	const data = {
+		from: `"New Password" <${process.env.USERNAME_EMAIL}>`,
+		to: email,
+		subject: 'Verification email code',
+		text: 'Your code to verification email',
+		html: `<h1>${randomPassword}</h1>`,
+	};
+
+	const user = await UserModel.findOne({ email });
+	if (user) {
+		const salt = await bcrypt.genSalt(10);
+
+        // hash randomPassword has to be a string not number
+		const hashedPassword = await bcrypt.hash(`${randomPassword}`, salt);
+
+		await UserModel.findByIdAndUpdate(user._id, {
+			password: hashedPassword,
+			isChangePassword: true,
+		})
+			.then(() => {
+				console.log('Done');
+			})
+			.catch((error) => console.log(error));
+
+		await handleSendMail(data)
+			.then(() => {
+				res.status(200).json({
+					message: 'Send email new password successfully!!!',
+					data: [],
+				});
+			})
+			.catch((error) => {
+				res.status(401);
+				throw new Error('Can not send email');
+			});
+	} else {
+		res.status(401);
+		throw new Error('User not found!!!');
+	}
+});
+
 module.exports = {
     register,
     login,
     verification,
+    forgotPassword
 }
